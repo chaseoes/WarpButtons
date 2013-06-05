d9f43585ed8618775f5a89ba597de03aa565e221
Patrick Seidel <psycowithespn1@aim.com>
2013-06-04 22:38:10 -0400

    There you go chase

--- src/main/java/me/chaseoes/warpbuttons/Warp.java
+++ src/main/java/me/chaseoes/warpbuttons/Warp.java
@@ -2,70 +2,138 @@ package me.chaseoes.warpbuttons;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Warp {
-	
-	String name;
-	
-	public Warp(String n) {
-		name = n;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public boolean exists() {
-		return getLocation() != null;
-	}
-	
-	public boolean linkedButtonExists() {
-		return getLinkedButton() != null;
-	}
-	
-	public Location getLocation() {
-		String s = WarpButtons.getInstance().getConfig().getString("warps." + getName() + ".location");
-		if (s != null) {
-			return loadLocation(s);
-		}
-		return null;
-	}
-	
-	public Location getLinkedButton() {
-		String s = WarpButtons.getInstance().getConfig().getString("warps." + getName() + ".linked-button");
-		if (s != null) {
-			return loadLocation(s);
-		}
-		return null;
-	}
-	
-	public void setLocation(Location l) {
-		WarpButtons.getInstance().getConfig().set("warps." + getName() + ".location", l.getWorld().getName() + "," + l.getBlockX() + "," + l.getBlockY() + "," + l.getBlockZ());
-		WarpButtons.getInstance().saveConfig();
-	}
-	
-	public void setLinkedButton(Location l) {
-		WarpButtons.getInstance().getConfig().set("warps." + getName() + ".linked-button", l.getWorld().getName() + "," + l.getBlockX() + "," + l.getBlockY() + "," + l.getBlockZ());
-		WarpButtons.getInstance().saveConfig();
-	}
-	
-	public void teleport(Player player) {
-		player.teleport(getLocation().add(0.5, 0, 0.5));
-	}
-	
-	private Location loadLocation(String s) {
-		String[] sp = s.split(",");
-		return new Location(WarpButtons.getInstance().getServer().getWorld(sp[0]), Integer.parseInt(sp[1]), Integer.parseInt(sp[2]), Integer.parseInt(sp[3]));
-	}
-	
-	public void delete() {
-		if (exists()) {
-			WarpButtons.getInstance().getConfig().set("warps." + getName() + ".location", null);
-			if (linkedButtonExists()) {
-				WarpButtons.getInstance().getConfig().set("warps." + getName() + ".linked-button", null);
-			}
-			WarpButtons.getInstance().saveConfig();
-		}
-	}
 
+    String name;
+    List<Location> buttonCache = new ArrayList<Location>();
+    FixedMetadataValue metadata;
+
+    public Warp(String n) {
+        name = n;
+        metadata = new FixedMetadataValue(WarpButtons.getInstance(), this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean exists() {
+        return getLocation() != null;
+    }
+
+    public boolean linkedButtonExists() {
+        return getLinkedButtons().size() != 0;
+    }
+
+    public Location getLocation() {
+        String s = WarpButtons.getInstance().getConfig().getString("warps." + getName() + ".location");
+        if (s != null) {
+            return loadLocation(s);
+        }
+        return null;
+    }
+
+    public List<Location> getLinkedButtons() {
+        if (buttonCache == null) {
+            List<String> s = WarpButtons.getInstance().getConfig().getStringList("warps." + getName() + ".linked-buttons");
+            List<Location> locs = new ArrayList<Location>();
+            if (s != null) {
+                for (String str : s) {
+                    locs.add(loadLocation(str));
+                }
+            }
+            buttonCache = locs;
+        }
+        return buttonCache;
+    }
+
+    public void setLocation(Location l) {
+        WarpButtons.getInstance().getConfig().set("warps." + getName() + ".location", saveLocation(l));
+        WarpButtons.getInstance().saveConfig();
+    }
+
+    public boolean addLinkedButton(Location l) {
+        List<String> locs = WarpButtons.getInstance().getConfig().getStringList("warps." + getName() + ".linked-buttons");
+        l = normalize(l);
+        boolean flag = true;
+        for (int i = 0; i < locs.size(); i++) {
+            if (compareLocations(loadLocation(locs.get(i)), l)) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            locs.add(saveLocation(l));
+            WarpButtons.getInstance().getConfig().set("warps." + getName() + ".linked-buttons", locs);
+            WarpButtons.getInstance().saveConfig();
+            l.getBlock().setMetadata("warpbuttons", metadata);
+        }
+        return flag;
+    }
+
+    public boolean removeLinkedButton(Location l) {
+        List<String> locs = WarpButtons.getInstance().getConfig().getStringList("warps." + getName() + ".linked-buttons");
+        l = normalize(l);
+        boolean flag = false;
+        for (int i = 0; i < locs.size(); i++) {
+            if (compareLocations(loadLocation(locs.get(i)), l)) {
+                flag = true;
+                locs.remove(i);
+            }
+        }
+        if (flag) {
+            WarpButtons.getInstance().getConfig().set("warps." + getName() + ".linked-buttons", locs);
+            WarpButtons.getInstance().saveConfig();
+            l.getBlock().removeMetadata("warpbuttons", WarpButtons.getInstance());
+        }
+        return flag;
+    }
+
+    public void teleport(Player player) {
+        Location loc = getLocation();
+        if (loc != null) {
+            player.teleport(getLocation().add(0.5, 0, 0.5));
+        }
+    }
+
+    private Location loadLocation(String s) {
+        String[] sp = s.split(",");
+        return new Location(WarpButtons.getInstance().getServer().getWorld(sp[0]), Integer.parseInt(sp[1]), Integer.parseInt(sp[2]), Integer.parseInt(sp[3]));
+    }
+
+    private String saveLocation(Location l) {
+        return l.getWorld().getName() + "," + l.getBlockX() + "," + l.getBlockY() + "," + l.getBlockZ();
+    }
+
+    private Location normalize(Location l) {
+        return new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ());
+    }
+
+    public void delete() {
+        if (exists()) {
+            WarpButtons.getInstance().getConfig().set("warps." + getName() + ".location", null);
+            if (linkedButtonExists()) {
+                WarpButtons.getInstance().getConfig().set("warps." + getName() + ".linked-button", null);
+            }
+            WarpButtons.getInstance().saveConfig();
+        }
+    }
+
+    public void invalidateCache() {
+        buttonCache = null;
+    }
+
+    public void initMetadata() {
+        for (Location loc : getLinkedButtons()) {
+            loc.getBlock().setMetadata("warpbuttons", metadata);
+        }
+    }
+
+    public boolean compareLocations(Location l1, Location l2) {
+        return (l1.getWorld().getName().equals(l2.getWorld().getName()) && l1.getBlockX() == l2.getBlockX() && l1.getBlockY() == l2.getBlockY() && l1.getBlockZ() == l2.getBlockZ());
+    }
 }
--- src/main/java/me/chaseoes/warpbuttons/WarpButtons.java
+++ src/main/java/me/chaseoes/warpbuttons/WarpButtons.java
@@ -6,107 +6,161 @@ import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WarpButtons extends JavaPlugin implements Listener {
 
-	private static WarpButtons instance;
+    private static WarpButtons instance;
+    private List<Warp> warps = new ArrayList<Warp>();
 
-	public static WarpButtons getInstance() {
-		return instance;
-	}
+    public static WarpButtons getInstance() {
+        return instance;
+    }
 
-	public void onEnable() {
-		instance = this;
-		getServer().getPluginManager().registerEvents(this, this);
-	}
+    public void onEnable() {
+        instance = this;
+        getServer().getPluginManager().registerEvents(this, this);
+        if (getConfig().isConfigurationSection("warps")) {
+            for (String s : getConfig().getConfigurationSection("warps").getKeys(false)) {
+                Warp warp = new Warp(s);
+                warp.initMetadata();
+                warps.add(warp);
+            }
+        }
+    }
 
-	public void onDisable() {
-		reloadConfig();
-		saveConfig();
-		instance = null;
-	}
+    public void onDisable() {
+        reloadConfig();
+        saveConfig();
+        instance = null;
+    }
 
-	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
-		if (cmnd.getName().equalsIgnoreCase("warpbuttons")) {
-			if (strings.length == 0) {
-				cs.sendMessage(ChatColor.AQUA + "WarpButtons version " + getDescription().getVersion() + " by chaseoes.");
-				return true;
-			}
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+        if (cmnd.getName().equalsIgnoreCase("warpbuttons")) {
+            if (strings.length == 0) {
+                cs.sendMessage(ChatColor.AQUA + "WarpButtons version " + getDescription().getVersion() + " by chaseoes.");
+                return true;
+            }
+            if (cs.hasPermission("warpbuttons.admin")) {
+                if (strings[0].equalsIgnoreCase("create")) {
+                    if (cs instanceof Player) {
+                        if (strings.length == 2) {
+                            Warp warp = new Warp(strings[1]);
+                            warp.setLocation(((Player) cs).getLocation());
+                            cs.sendMessage(ChatColor.AQUA + "Successfully created the warp " + strings[1] + "!");
+                        } else {
+                            cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons create <name>");
+                        }
+                    } else {
+                        cs.sendMessage("You must be a player to do that.");
+                    }
+                } else if (strings[0].equalsIgnoreCase("delete") || strings[0].equalsIgnoreCase("remove")) {
+                    if (strings.length == 2) {
+                        Warp warp = new Warp(strings[1]);
+                        warp.delete();
+                        cs.sendMessage(ChatColor.AQUA + "Successfully deleted the warp " + strings[1] + "!");
+                    } else {
+                        cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons delete <name>");
+                    }
+                } else if (strings[0].equalsIgnoreCase("link")) {
+                    if (cs instanceof Player) {
+                        Player player = (Player) cs;
+                        if (strings.length > 1) {
+                            if (strings[1].equalsIgnoreCase("add")) {
+                                Warp warp = new Warp(strings[2]);
+                                if (warp.exists()) {
+                                    if (warp.addLinkedButton(player.getTargetBlock(null, 5).getLocation())) {
+                                        cs.sendMessage(ChatColor.AQUA + "Successfully linked to the warp " + strings[1] + "!");
+                                    } else {
+                                        cs.sendMessage(ChatColor.RED + "Button is already linked");
+                                    }
+                                } else {
+                                    cs.sendMessage(ChatColor.RED + "That warp does not exist!");
+                                }
+                            } else if (strings[1].equalsIgnoreCase("remove")) {
+                                Block b = player.getTargetBlock(null, 5);
+                                if (b == null) {
+                                    cs.sendMessage(ChatColor.RED + "Button not targeted");
+                                } else {
+                                    if (b.hasMetadata("warpbuttons")) {
+                                        Warp warp = (Warp) b.getMetadata("warpbuttons").get(0).value();
+                                        if (warp.removeLinkedButton(b.getLocation())) {
+                                            cs.sendMessage(ChatColor.AQUA + "Unlinked button");
+                                        } else {
+                                            cs.sendMessage(ChatColor.RED + "Could not unlink button");
+                                        }
+                                    } else {
+                                        cs.sendMessage(ChatColor.RED + "Button is not linked");
+                                    }
+                                }
+                            } else {
+                                cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons link [add|remove] [name]");
+                            }
 
-			if (strings[0].equalsIgnoreCase("create")) {
-				if (cs instanceof Player) {
-					if (strings.length == 2) {
-						Warp warp = new Warp(strings[1]);
-						warp.setLocation(((Player)cs).getLocation());
-						cs.sendMessage(ChatColor.AQUA + "Successfully created the warp " + strings[1] + "!");
-					} else {
-						cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons create <name>");
-					}
-				} else {
-					cs.sendMessage("You must be a player to do that.");
-				}
-			}
+                        } else {
+                            cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons link [add|remove] [name]");
+                        }
+                    } else {
+                        cs.sendMessage("You must be a player to do that.");
+                    }
+                } else {
+                    cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons link [add|remove] [name]");
+                }
+            } else {
+                cs.sendMessage(ChatColor.RED + "You don't have permission to do that");
+            }
+        }
+        return true;
+    }
 
-			if (strings[0].equalsIgnoreCase("delete") || strings[0].equalsIgnoreCase("remove")) {
-				if (strings.length == 2) {
-					Warp warp = new Warp(strings[1]);
-					warp.delete();
-					cs.sendMessage(ChatColor.AQUA + "Successfully deleted the warp " + strings[1] + "!");
-				} else {
-					cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons delete <name>");
-				}
-			}
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.hasBlock() && (event.getClickedBlock().getType() == Material.STONE_BUTTON || event.getClickedBlock().getType() == Material.WOOD_BUTTON) && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (event.getClickedBlock().hasMetadata("warpbuttons")) {
+                List<MetadataValue> vals = event.getClickedBlock().getMetadata("warpbuttons");
+                FixedMetadataValue val = (FixedMetadataValue) vals.get(0);
+                Warp warp = (Warp) val.value();
+                warp.teleport(event.getPlayer());
+            }
+        }
+    }
 
-			if (strings[0].equalsIgnoreCase("link")) {
-				if (cs instanceof Player) {
-					Player player = (Player) cs;
-				if (strings.length == 2) {
-					Warp warp = new Warp(strings[1]);
-					if (warp.exists()) {
-						warp.setLinkedButton(player.getTargetBlock(null, 5).getLocation());
-						cs.sendMessage(ChatColor.AQUA + "Successfully linked to the warp " + strings[1] + "!");
-					} else {
-						cs.sendMessage(ChatColor.RED + "That warp does not exist!");
-					}
-				} else {
-					cs.sendMessage(ChatColor.RED + "Usage: /warpbuttons link <name>");
-				}
-				} else {
-					cs.sendMessage("You must be a player to do that.");
-				}
-			}
-		}
-		return true;
-	}
-	
-	@EventHandler
-	public void onInteract(PlayerInteractEvent event) {
-		if (event.hasBlock() && (event.getClickedBlock().getType() == Material.STONE_BUTTON || event.getClickedBlock().getType() == Material.WOOD_BUTTON)) {
-			for (Warp w : getWarps()) {
-				if (compareLocations(w.getLinkedButton(), event.getClickedBlock().getLocation())) {
-					w.teleport(event.getPlayer());
-				}
-			}
-		}
-	}
-	
-	public List<Warp> getWarps() {
-		List<Warp> warps = new ArrayList<Warp>();
-		for (String s : getConfig().getConfigurationSection("warps").getKeys(false)) {
-			warps.add(new Warp(s));
-		}
-		return warps;
-	}
-	
-	public boolean compareLocations(Location l1, Location l2) {
-		return (l1.getWorld().getName().equals(l2.getWorld().getName()) && l1.getBlockX() == l2.getBlockX() && l1.getBlockY() == l2.getBlockY() && l1.getBlockZ() == l2.getBlockZ());
-	}
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (event.getBlock().getType() == Material.STONE_BUTTON || event.getBlock().getType() == Material.WOOD_BUTTON) {
+            if (event.getBlock().hasMetadata("warpbuttons")) {
+                if (!event.getPlayer().hasPermission("warpbuttons.admin")) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(ChatColor.RED + "You can't break a Warp Button");
+                } else {
+                    Warp warp = (Warp) event.getBlock().getMetadata("warpbuttons").get(0).value();
+                    if (warp.removeLinkedButton(event.getBlock().getLocation())) {
+                        event.getPlayer().sendMessage(ChatColor.AQUA + "Unlinked button");
+                    } else {
+                        event.getPlayer().sendMessage(ChatColor.RED + "Could not unlink button");
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Warp> getWarps() {
+        return warps;
+    }
+
+    public boolean compareLocations(Location l1, Location l2) {
+        return (l1.getWorld().getName().equals(l2.getWorld().getName()) && l1.getBlockX() == l2.getBlockX() && l1.getBlockY() == l2.getBlockY() && l1.getBlockZ() == l2.getBlockZ());
+    }
 
 }
